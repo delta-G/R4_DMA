806e959ab16c4b881c04fb5ccb142feb715c9155
David <6957239+delta-G@users.noreply.github.com>
2023-10-28 21:06:00 -0500

    prints all registers

--- R4_DMA_Test.ino
+++ R4_DMA_Test.ino
@@ -1,7 +1,87 @@
+#define PRINT_REG(BUCKET, REGISTER) \
+  do { \
+    uint32_t t = BUCKET->REGISTER; \
+    Serial.print(#REGISTER " : 0x"); \
+    Serial.println(t, HEX); \
+  } while (false)
+
+
 void setup() {
 
+  Serial.begin(115200);
+  while (!Serial)
+    ;
+  Serial.println("\n\n\nStarting R4_DMA_Test.ino");
+
+  printRegisters(0);
+  printRegisters(1);
+  printRegisters(2);
+  printRegisters(3);
+
+
+  Serial.println("End Setup");
 }
 
 void loop() {
+}
 
+void printRegisters(uint8_t ch) {
+  Serial.print("\nPrinting Registers for channel ");
+  Serial.println(ch);
+
+  PRINT_REG(R_DMA, DMAST);
+  switch (ch) {
+    case 0:
+      PRINT_REG(R_DMAC0, DMSAR);
+      PRINT_REG(R_DMAC0, DMDAR);
+      PRINT_REG(R_DMAC0, DMCRA);
+      PRINT_REG(R_DMAC0, DMCRB);
+      PRINT_REG(R_DMAC0, DMTMD);
+      PRINT_REG(R_DMAC0, DMINT);
+      PRINT_REG(R_DMAC0, DMAMD);
+      PRINT_REG(R_DMAC0, DMOFR);
+      PRINT_REG(R_DMAC0, DMCNT);
+      PRINT_REG(R_DMAC0, DMREQ);
+      PRINT_REG(R_DMAC0, DMSTS);
+      break;
+    case 1:
+      PRINT_REG(R_DMAC1, DMSAR);
+      PRINT_REG(R_DMAC1, DMDAR);
+      PRINT_REG(R_DMAC1, DMCRA);
+      PRINT_REG(R_DMAC1, DMCRB);
+      PRINT_REG(R_DMAC1, DMTMD);
+      PRINT_REG(R_DMAC1, DMINT);
+      PRINT_REG(R_DMAC1, DMAMD);
+      PRINT_REG(R_DMAC1, DMOFR);
+      PRINT_REG(R_DMAC1, DMCNT);
+      PRINT_REG(R_DMAC1, DMREQ);
+      PRINT_REG(R_DMAC1, DMSTS);
+      break;
+    case 2:
+      PRINT_REG(R_DMAC2, DMSAR);
+      PRINT_REG(R_DMAC2, DMDAR);
+      PRINT_REG(R_DMAC2, DMCRA);
+      PRINT_REG(R_DMAC2, DMCRB);
+      PRINT_REG(R_DMAC2, DMTMD);
+      PRINT_REG(R_DMAC2, DMINT);
+      PRINT_REG(R_DMAC2, DMAMD);
+      PRINT_REG(R_DMAC2, DMOFR);
+      PRINT_REG(R_DMAC2, DMCNT);
+      PRINT_REG(R_DMAC2, DMREQ);
+      PRINT_REG(R_DMAC2, DMSTS);
+      break;
+    case 3:
+      PRINT_REG(R_DMAC3, DMSAR);
+      PRINT_REG(R_DMAC3, DMDAR);
+      PRINT_REG(R_DMAC3, DMCRA);
+      PRINT_REG(R_DMAC3, DMCRB);
+      PRINT_REG(R_DMAC3, DMTMD);
+      PRINT_REG(R_DMAC3, DMINT);
+      PRINT_REG(R_DMAC3, DMAMD);
+      PRINT_REG(R_DMAC3, DMOFR);
+      PRINT_REG(R_DMAC3, DMCNT);
+      PRINT_REG(R_DMAC3, DMREQ);
+      PRINT_REG(R_DMAC3, DMSTS);
+      break;
+  }
 }
